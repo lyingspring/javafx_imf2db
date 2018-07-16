1169badb3feaf9fc5428a1f2e3c158f7d61f6b97
lyingspring <31447709+lyingspring@users.noreply.github.com>
2018-07-17 02:26:34 +0800

    add ico

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("文件导入到数据库");
         primaryStage.setScene(new Scene(root, 600, 550));
+        primaryStage.getIcons().add(new Image(//窗口左上角图标
+                Main.class.getResourceAsStream("/img/ico.jpg")));
         primaryStage.show();
         System.out.println("当前程序路径 "+System.getProperty("user.dir").toString());
     }
